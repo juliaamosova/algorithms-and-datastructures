1572476c73921831b36c3ce8a2908871b052c509
Julia Amosova <julia.amosova@gmail.com>
2018-08-03 21:05:22 -0400

    Create index.js

---
+++ chunk_array/iteration/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+// Solution:
+// - Create an empty array to hold chunks called 'chunked'
+// - For each element in the *unchunked* array
+// ---- Retrieve the last element in 'chunked'
+// ---- If last element doesn't exist, or if its lenght is equal to chunk size
+// -------- Push a new chunk into 'chunked' with the current element
+// ---- Else add the current element into the chunk
+
+
+function chunk(array, size) {
+
+      const chunked = [];
+
+      for (let element of array) {
+          const last = chunked[chunked.length - 1];
+          if (!last || last.length === size) {
+              chunked.push([element]);
+          } else {
+              last.push(element);
+          }
+      }
+
+      return chunked;
+}
